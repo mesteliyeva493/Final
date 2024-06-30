039fb5bc3d11b7a99d4ee2c37476513d095bc84d
mesteliyeva493 <mesteliyeva004@gmail.com>
2024-06-30 19:10:59 +0400

    admin

--- front/src/Pages/Admin/Product/Product.jsx
+++ front/src/Pages/Admin/Product/Product.jsx
@@ -1,5 +1,68 @@
-import React, { useContext } from 'react'
-import './Product.scss'
+// import React, { useContext } from 'react'
+// import './Product.scss'
+// import Table from '@mui/material/Table';
+// import TableBody from '@mui/material/TableBody';
+// import TableCell from '@mui/material/TableCell';
+// import TableContainer from '@mui/material/TableContainer';
+// import TableHead from '@mui/material/TableHead';
+// import TableRow from '@mui/material/TableRow';
+// import Paper from '@mui/material/Paper';
+// import mainContext from '../../../Context/Context';
+// import { Button } from '@mui/material';
+// function Product() {
+//     const { product,deletetProduct } = useContext(mainContext);
+
+//     return (
+//         <>
+//             <TableContainer component={Paper}>
+//                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
+//                     <TableHead>
+//                         <TableRow>
+//                             <TableCell>#</TableCell>
+//                             <TableCell align="right">image</TableCell>
+//                             <TableCell align="right">name</TableCell>
+//                             <TableCell align="right">title</TableCell>
+//                             <TableCell align="right">price</TableCell>
+//                             <TableCell align="right">Delete</TableCell>
+//                             <TableCell align="right">Edit</TableCell>
+//                         </TableRow>
+//                     </TableHead>
+//                     <TableBody>
+//                         {product.map((item, index) => (
+//                             <TableRow
+//                                 key={index}
+//                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+//                             >
+//                                 <TableCell component="th" scope="row">
+//                                     {index}
+//                                 </TableCell>
+//                                 <TableCell align="right"><img width={100} height={100} src={item.img} alt="" /></TableCell>
+//                                 <TableCell align="right">{item.name}</TableCell>
+//                                 <TableCell align="right">{item.title}</TableCell>
+//                                 <TableCell align="right">{item.price}</TableCell>
+//                                 <TableCell align="right">  <Button onClick={()=>{
+//                                     deletetProduct(item._id)
+//                                 }}>Delete</Button></TableCell>
+
+//                                 <TableCell align="right"><Button>Edit</Button></TableCell>
+
+//                             </TableRow>
+//                         ))}
+//                     </TableBody>
+//                 </Table>
+//             </TableContainer>
+
+
+
+
+//         </>
+//     )
+// }
+
+// export default Product
+
+import React, { useContext } from 'react';
+import './Product.scss';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,54 +72,50 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import mainContext from '../../../Context/Context';
 import { Button } from '@mui/material';
+
 function Product() {
-    const { product,deletetProduct } = useContext(mainContext);
+  const { product, deletetProduct } = useContext(mainContext);
 
-    return (
-        <>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>#</TableCell>
-                            <TableCell align="right">image</TableCell>
-                            <TableCell align="right">name</TableCell>
-                            <TableCell align="right">title</TableCell>
-                            <TableCell align="right">price</TableCell>
-                            <TableCell align="right">Delete</TableCell>
-                            <TableCell align="right">Edit</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {product.map((item, index) => (
-                            <TableRow
-                                key={index}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    {index}
-                                </TableCell>
-                                <TableCell align="right"><img width={100} height={100} src={item.img} alt="" /></TableCell>
-                                <TableCell align="right">{item.name}</TableCell>
-                                <TableCell align="right">{item.title}</TableCell>
-                                <TableCell align="right">{item.price}</TableCell>
-                                <TableCell align="right">  <Button onClick={()=>{
-                                    deletetProduct(item._id)
-                                }}>Delete</Button></TableCell>
-
-                                <TableCell align="right"><Button>Edit</Button></TableCell>
-
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-
-
-
-
-        </>
-    )
+  return (
+    <section id='Product'>
+      <TableContainer component={Paper} className="table-container">
+        <Table sx={{ minWidth: 800 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>#</TableCell>
+              <TableCell align="right">Image</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Title</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Delete</TableCell>
+              <TableCell align="right">Edit</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {product.map((item, index) => (
+              <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell component="th" scope="row">
+                  {index + 1}
+                </TableCell>
+                <TableCell align="right">
+                  <img width={100} height={100} src={item.img} alt="" />
+                </TableCell>
+                <TableCell align="right">{item.name}</TableCell>
+                <TableCell align="right">{item.title}</TableCell>
+                <TableCell align="right">{item.price}</TableCell>
+                <TableCell align="right">
+                  <Button onClick={() => deletetProduct(item._id)}>Delete</Button>
+                </TableCell>
+                <TableCell align="right">
+                  <Button>Edit</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </section>
+  );
 }
 
-export default Product
+export default Product;
